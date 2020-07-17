ecb8d6e9d66aa2aeb6061e626dea8ab117efcd7f
Mohamed El Béchir <mohamedelbechir1@gmail.com>
2020-07-17 06:41:45 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientComponent } from './client/client.component';
+import { ClientDetailsComponent } from './client/client-details/client-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  ...['', 'clients'].map((path) => ({ path, component: ClientComponent })),
+  {
+    path: 'client-details',
+    component: ClientDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ClientComponent } from './client/client.component';
+import { ClientListItemComponent } from './client/client-list-item/client-list-item.component';
+import { ClientListingPageComponent } from './client/client-listing-page/client-listing-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ClientDetailsComponent } from './client/client-details/client-details.component';
+import { ReactiveFormsModule }  from '@angular/forms';
+import { ConversationComponent } from './conversation/conversation.component';
+import { BesoinComponent } from './besoin/besoin.component';
+import { BesoinListItemPrestaComponent } from './besoin/besoin-list-item-presta/besoin-list-item-presta.component';
+import { PrestataireComponent } from './prestataire/prestataire.component';
+import { AfficherFichierComponent } from './afficher-fichier/afficher-fichier.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ClientListItemComponent,
+    ClientListingPageComponent,
+    ClientDetailsComponent,
+    ConversationComponent,
+    BesoinComponent,
+    BesoinListItemPrestaComponent,
+    PrestataireComponent,
+    AfficherFichierComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
